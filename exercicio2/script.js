1729229e7e010840c7645c5db26597b436545a21
falcaomm <manu.m.falcao@gmail.com>
2022-08-09 18:08:53 -0300

    Exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,21 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase()
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+switch (nacionalidade) {
+    case "brasileira":
+        console.log(nacionalidade);
+        break;
+    case "argentina":
+        console.log(nacionalidade);
+        break;
+    case "uruguaia":
+        console.log(nacionalidade);
+        break;
+    case "chilena":
+        console.log(nacionalidade);
+        break;
+    case "colombiana":
+        console.log(nacionalidade);
+        break;
+    default:
+        console.log("nacionalidade não encontrada");
 }
